22b65d4392768681e1529d5fa02185bd8cc7d01e
Gabriele Cavigiolo <gabriele.cavigiolo@synesthesia.it>
2021-06-18 15:50:26 +0200

    conflict merged

--- gabricavi/_sources/setup-scripts/aws-cli-credential.sh
+++
@@ -1,3 +0,0 @@
-# Comando da lanciare dopo aver creato le credenziali su aws console
-
-serverless config credentials --provider aws --key AKIAQUALCOSA --secret stringasegretaincomprensibile --profile serverless-admin
