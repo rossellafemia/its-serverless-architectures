e20c2241207454c5d0a87665cdb3bb555cc85896
Gabriele Cavigiolo <gabri.cavi@gmail.com>
2021-07-12 11:36:55 +0200

    scaling 2 (#102)
    * Conflitto 1
    
    * git checkout main
    
    * Added azure code
    
    * Added project s3
    
    * Added gitignore
    
    * Added scaling
    
    Co-authored-by: Gabriele Cavigiolo <gabriele.cavigiolo@synesthesia.it>
    Co-authored-by: Gabriele Cavigiolo <61781659+gabricavisyn@users.noreply.github.com>

--- _sources/_homeworks/create-movie-archive/movies/list.js
+++ _sources/_homeworks/create-movie-archive/movies/list.js
@@ -3,30 +3,37 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 module.exports.list = (event, context, callback) => {
 
-  const params = {
-    TableName: process.env.MOVIES_TABLE
-  };
 
-  // fetch all todos from the database
-  dynamoDb.scan(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todos.',
-      });
-      return;
-    }
-
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Items),
+  sleep(5000).then(() => {
+    const params = {
+      TableName: process.env.MOVIES_TABLE
     };
-    callback(null, response);
-  });
+  
+    // fetch all todos from the database
+    dynamoDb.scan(params, (error, result) => {
+      // handle potential errors
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Couldn\'t fetch the todos.',
+        });
+        return;
+      }
+  
+      // create a response
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Items),
+      };
+      callback(null, response);
+    });
+  })  
 };
