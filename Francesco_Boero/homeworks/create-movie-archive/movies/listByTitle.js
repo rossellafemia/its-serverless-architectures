1015be75ba2b2acbbd33eb0dbb7a0d59c65c8834
Bobo <39982810+BOBOfra@users.noreply.github.com>
2021-07-13 09:50:37 +0200

    movies - consegna esercitazione (#90)
    * Create Cloud_specialist.php
    
    Say "hi" to the entire world
    
    * BOBOfra commit
    
    * Update readme.md
    
    * Update changelog.md
    
    * Rename changelog.md to CHANGELOG.md
    
    * Rename readme.md to README.md
    
    * "sources" folder copied in my personal folder
    
    * folder Fracesco Boero renamed
    
    * Added todo_rich.drawio
    
    * todos_rich added to service pattern
    
    * Attribute "price" added to todo_rich
    
    * pattern-monolitich updated for todos_rich
    
    * correzione
    
    * Update todo_rich.drawio
    
    * added function todosRichHandler in yml file
    
    * fix monolitich
    
    * hello-world-pytthon and aws-...-dynamodb DEPLOYED
    
    * package.json installed in aws-[...]-dynamodb
    
    * changed error from 200 to 201
    
    * fix fetchup stream 26
    
    * update
    
    * ennesimo fix
    
    * last fix
    
    * compito punti 1-4
    
    * compito eseguito da 1 a 4, punto 5 desctitto in README.md
    
    Co-authored-by: Gabriele Cavigiolo <61781659+gabricavisyn@users.noreply.github.com>

---
+++ Francesco_Boero/homeworks/create-movie-archive/movies/listByTitle.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.listByTitle = (event, context, callback) => {
+  const params = {
+    TableName: process.env.MOVIES_TABLE,
+    Key: {
+      title: event.pathParameters.title,
+    },
+  };
+
+  // fetch todo from the database
+  dynamoDb.query(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
